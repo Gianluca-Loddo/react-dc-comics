f8733d1f94cca00787f4efe9a2f57e6da640a73b
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-23 18:17:29 +0200

    fix 6 create code: index, app.jsx, main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header.jsx'
 import Jumbotron from './components/Jumbotron.jsx'
-import BlueBar from './components/BlueBar.jsx'       // Bonus
+import BlueBar from './components/BlueBar.jsx'   // Bonus richiesto
 import Footer from './components/Footer.jsx'
 
 export default function App() {
