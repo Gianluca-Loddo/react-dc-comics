a09f94bb435fbe39a4884bd36e48c08065255417
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-25 19:51:17 +0200

    map metod

--- TEST 2/src/App.jsx
+++ TEST 2/src/App.jsx
@@ -5,9 +5,33 @@ import Footer from './components/Footer.jsx'
 
 import './App.css'  // Importazione del file CSS
 
+
+const TEST = ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES"];
+const TEST_2 = [<li>CHARACTERS</li>, <li>COMICS</li>,<li>MOVIES</li>, <li>TV</li>, <li>GAMES</li>];
+
+
 export default function App() {
   return (
     <>
+      {/* esempi su come "stampare" dinamicamente una lista di array di oggetti o stringhe  fino ad arrivare al metodo MAP */}
+      
+      <div>
+        {TEST}
+      </div>
+
+       <ul>
+        {TEST_2}
+      </ul>
+
+      <ul>
+        {TEST.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+
+      {/* ================================================================================================================== */}
+      
+      
       <Header />
       <Jumbotron />
       <BlueBar />
