0d75f53fbb90ca03def4d097221565ce0192b5c2
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-26 18:55:24 +0200

    install Bootstrap & Fonts

--- TEST_LAST_VERSION/src/App.jsx
+++ TEST_LAST_VERSION/src/App.jsx
@@ -2,6 +2,10 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+// importo il font dopo averlo installato con il comando:
+// npm install @fontsource-variable/roboto
+
+import '@fontsource-variable/roboto';
 
 function App() {
  
