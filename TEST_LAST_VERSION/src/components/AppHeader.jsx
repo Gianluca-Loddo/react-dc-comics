3d54b56ad03985dd17ef648ae7e459fcf158edb4
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-26 18:48:54 +0200

    restart Last Version of react-dc-comics project

---
+++ TEST_LAST_VERSION/src/components/AppHeader.jsx
@@ -0,0 +1,9 @@
+import DcLogo from '../assets/dc-logo.png';
+
+export default function AppHeader() {
+  return (
+    <header>
+        <div className="container">
+          <img src={DcLogo} alt="DC Logo" />
+        </div>
+    </header>>
