8fdc1909e527253dcacab8dcee9b0df148328f44
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-29 18:27:20 +0200

    delete sub folder & move all files -> index folder

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,19 @@
+export default function AppFooter() {
+
+return (
+
+<footer>
+    <div className="backgroundImgFooter">
+        <div className="container"> {/*bootstrap riconosce container*/}
+            <div className="row">
+                <div className="col">
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+)}
