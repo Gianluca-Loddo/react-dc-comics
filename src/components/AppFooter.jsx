32f9e8696922c6348caef18dfd7c91bd33cf362c
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-09-30 07:43:00 +0200

    finish first part (footer)

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,20 +1,25 @@
+import footerData from "../data/footerData";
+
 export default function AppFooter() {
 
 return (
-
 <footer>
     <div className="backgroundImgFooter">
         <div className="container"> {/*bootstrap riconosce container*/}
             <div className="row">
-                <div className="col">
-                    
-
-
-                </div>
+                {footerData.map((titleFooter, idFooter) => (
+                    <div className="col" key={idFooter}> {/*gli passo l'id della colonna che corrisponde al primo id di footerData*/}
+                        <h6 className="text-light">{titleFooter.title}</h6> {/*gli passo il title di footerData*/}
+                        <ul>{titleFooter.list.map((labelFooter, id2Footer) => 
+                            <li key={id2Footer}> {/*gli passo l'id che corrisponde al secondo id di footerData*/}
+                               <a href="#">{labelFooter.label}</a>
+                            </li>
+                        )};
+                        </ul>
+                    </div>
+                ))};
             </div>
         </div>
     </div>
 </footer>
-
-
 )}
--- src/data/footerData.js
+++ src/data/footerData.js
@@ -1,6 +1,55 @@
 const footerData = [
-    "DC Comics",
-    "DC",
-    "SITES",
-    "SHOP"
+ {
+        id: 1,
+        title: "DC COMICS",
+        list: [
+
+            { id: 1, label: "Characters", url: "" },
+            { id: 2, label: "Comics", url: "" },
+            { id: 3, label: "Movies", url: "" },
+            { id: 4, label: "TV", url: "" },
+            { id: 5, label: "Games", url: "" },
+            { id: 6, label: "Videos", url: "" },
+            { id: 7, label: "News", url: "" },
+        ]
+    },
+
+    {
+        id: 2,
+        title: "DC",
+        list: [
+            { id: 1, label: "Terms Of Use", url: "" },
+            { id: 2, label: "Privacy policy(New)", url: "" },
+            { id: 3, label: "Ad Choises", url: "" },
+            { id: 4, label: "Advertising", url: "" },
+            { id: 5, label: "Jobs", url: "" },
+            { id: 6, label: "Subscription", url: "" },
+            { id: 7, label: "Talent Workshops", url: "" },
+            { id: 8, label: "CPSC Certificates", url: "" },
+            { id: 9, label: "Ratings", url: "" },
+            { id: 10, label: "Shop Help", url: "" },
+            { id: 11, label: "Contact Us", url: "" }
+        ]
+    },
+    {
+        id: 3,
+        title: "SITES",
+        list: [
+            { id: 1, label: "DC", url: "" },
+            { id: 2, label: "MAD Magazine", url: "" },
+            { id: 3, label: "DC Kids", url: "" },
+            { id: 4, label: "DC Universe", url: "" },
+            { id: 5, label: "DC Power Visa", url: "" }
+        ]
+    },
+    {
+        id: 4,
+        title: "SHOP",
+        list: [
+            { id: 1, label: "Shop DC", url: "" },
+            { id: 2, label: "Shop DC Collectibles", url: "" }
+        ]
+    }
 ];
+
+export default footerData;
